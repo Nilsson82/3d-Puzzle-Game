f9155c15c45da5b8fc7b1b0eabb0d23109ec6900
Nilsson82 <Anders858@gmail.com>
2018-12-30 21:38:08 +0100

    Created player movement and demo world.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private float maxSpeed = 5f;
+    private Vector3 input;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+        {
+            GetComponent<Rigidbody>().AddForce(input * moveSpeed);
+        }
+        
+    }
+}
