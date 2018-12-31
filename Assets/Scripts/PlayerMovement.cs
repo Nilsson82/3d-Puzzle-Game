697054ba6cc9b94c358ea9ecb060d9e47a6e1816
Nilsson82 <Anders858@gmail.com>
2018-12-31 06:51:52 +0100

    Added respawn function of player with 'Particle System' make player expload when hit by enemy.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public GameObject deathParticles;
 
     private float maxSpeed = 5f;
     private Vector3 input;
 
 
+    private Vector3 spawn;
+
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        spawn = transform.position;		
 	}
 	
 	// Update is called once per frame
@@ -26,5 +29,14 @@ public class PlayerMovement : MonoBehaviour
             GetComponent<Rigidbody>().AddForce(input * moveSpeed);
         }
         
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.transform.tag == "Enemy")
+        {
+            Instantiate(deathParticles, transform.position, Quaternion.identity);
+            transform.position = spawn;
+        }
     }
 }
