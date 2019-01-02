913ffa6b0cfb9c1d61088c34cfd7ca55dbcf661f
Nilsson82 <Anders858@gmail.com>
2019-01-02 14:48:55 +0100

    Change player skin to RubiksCube.

--- Assets/Scripts/MainManu.cs
+++ Assets/Scripts/MainManu.cs
@@ -12,14 +12,14 @@ public class MainManu : MonoBehaviour {
 
         GUI.skin = skin;
 
-        GUI.Label(new Rect(10, 10, 400, 75), "3D Puzzle Game");
+        GUI.Label(new Rect(100, 100, 400, 75), "3D Puzzle Game");
 
-        if (GUI.Button(new Rect(100, 100, 100, 45), "Play"))
+        if (GUI.Button(new Rect(100, 200, 100, 45), "Play"))
         {
             SceneManager.LoadScene(1);
         }
 
-        if (GUI.Button(new Rect(100, 150, 100, 45), "Quit"))
+        if (GUI.Button(new Rect(100, 250, 100, 45), "Quit"))
         {
             Application.Quit();
         }
